d331d3f990670b4791133356d6ebf4fcd4a5faea
Arunapriyakaligotla <159990605+Arunapriyakaligotla@users.noreply.github.com>
2024-03-21 20:11:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import Tabledata2 from './Tabledata2';
+
+function App() {
+  return (
+    <div>
+ 
+    <Tabledata2/>
+    </div>
+  );
+}
+
+export default App;
